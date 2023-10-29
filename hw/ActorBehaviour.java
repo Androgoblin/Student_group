9b16ac0e95f44b7cea42fc90e31a08bd9ba5fe20
Androgoblin <134535734+Androgoblin@users.noreply.github.com>
2023-10-29 10:53:17 +0300

    Delete hw directory

--- hw/ActorBehaviour.java
+++
@@ -1,9 +0,0 @@
-package hw;
-
-public interface ActorBehaviour {
-    void setMakeOrder(boolean makeOrder);
-    void setTakeOrder(boolean pickUpOrder);
-    boolean isMakeOrder();
-    boolean isTakeOrder();
-}
-
--- hw/QueueBehaviour.java
+++
@@ -1,9 +0,0 @@
-package hw;
-
-public interface QueueBehaviour {
-    
-    void takeInQueue(Actor actor);
-    void takeOrders();
-    void giveOrders();
-    void releaseFromQueue();
-}
